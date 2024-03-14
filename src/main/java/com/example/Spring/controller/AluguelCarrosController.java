b8245d5220c58f98013e166b7f171b4c2d43f74c
Luciano <luciano.coelhho@gmail>
2024-03-13 23:19:33 -0300

    delete

--- src/main/java/com/example/Spring/controller/AluguelCarrosController.java
+++ src/main/java/com/example/Spring/controller/AluguelCarrosController.java
@@ -2,8 +2,11 @@ package com.example.Spring.controller;
 
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,5 +38,11 @@ public class AluguelCarrosController {
     public AluguelCarro alterar(@RequestBody AluguelCarro aluguelCarro){
         return aluguelCarroService.alterar(aluguelCarro);
     }
-    
+
+    @DeleteMapping("/{id}delete")
+    public ResponseEntity<Void> delete(@PathVariable("id") long id){
+        aluguelCarroService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
